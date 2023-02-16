8674c66668e65b5b231b08832589ef36ff47beb4
Chris West (Faux) <git@goeswhere.com>
2023-02-16 12:43:58 +0000

    feat: slightly more viz pruning

--- scripts/viz.ts
+++ scripts/viz.ts
@@ -7,6 +7,11 @@ import { tupleToColon } from '../web/muffler/colon';
 initOnNode();
 
 function main() {
+  const banned = new Set([
+    'empty-barrel',
+    'py-storehouse-basic',
+    'combustion-mixture',
+  ]);
   console.log('digraph {rankdir="LR";');
   const included = new Set<string>();
   for (const [, obj] of Object.entries(data.doc)) {
@@ -17,15 +22,15 @@ function main() {
     }
   }
   for (const [name, obj] of Object.entries(data.doc)) {
-    const block = `${name} (${obj.tags.join(',')})`;
+    const block = `${name} (${obj.tags.join(',').replace(/"/g, "'")})`;
     for (const stop of obj.stop) {
       for (const [type, item] of stop.provides) {
-        if (item === 'empty-barrel') continue;
+        if (banned.has(item)) continue;
         console.log(`  "${block}" -> "${type}:${item}"`);
       }
 
       for (const [type, item, value] of stop.combinator) {
-        if (item === 'empty-barrel') continue;
+        if (banned.has(item)) continue;
         if (type !== 'virtual' && value < 0) {
           const key = tupleToColon([type, item]);
           if (included.has(key)) {
