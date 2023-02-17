b730115cfb593eb7f2708839c09d873a5760c5c7
Chris West (Faux) <git@goeswhere.com>
2023-02-17 16:38:52 +0000

    chore: factor items page

--- web/pages/objects.tsx
+++ web/pages/objects.tsx
@@ -26,6 +26,123 @@ export class IoFDetail extends Component<{
         </span>
       );
 
+    const colon = objToColon(props);
+
+    return (
+      <div class="container-fluid">
+        <h2>
+          <ItemIcon name={props.name} alt={props.name} /> {obj.localised_name}
+        </h2>
+        <p>
+          type: <span className="font-monospace">{props.type}</span>;
+          internal-name:<span class="font-monospace">{props.name}</span>; group:
+          <span class="font-monospace">{obj.group?.name}</span>; subgroup:
+          <span class="font-monospace">{obj.subgroup?.name}</span>.
+        </p>
+        <p>
+          <a href={`/ltn-tree/${props.type}/${props.name}`}>
+            Debug availability
+          </a>
+        </p>
+        <h3>LTN availability:</h3>
+        <LtnProvides colon={colon} />
+        <h3>LTN requests:</h3>
+        <LtnRequests colon={colon} />
+        <h3>Storage:</h3>
+        <Storage type={props.type} name={props.name} />
+        <h3>Ways to make:</h3>
+        <RecipeUsage type={props.type} name={props.name} />
+      </div>
+    );
+  }
+}
+
+class LtnProvides extends Component<{ colon: string }> {
+  render(props: { colon: string }) {
+    const providers = stations()
+      .flatMap(([loc, stop]) => {
+        const actualItemsAvailable = colonMapItems(stop)[props.colon];
+        if (!(actualItemsAvailable > 0)) {
+          return [];
+        }
+
+        const settings = settingsMap(stop);
+        const min = ltnMinTransfer(props.colon, settings);
+        return [[[loc, stop], actualItemsAvailable, min] as const];
+      })
+      .sort(
+        ([, avalue, amin], [, bvalue, bmin]) => bvalue / bmin - avalue / amin,
+      );
+
+    return (
+      <table className="ltn-avail">
+        {providers.map(([stop, value, min]) => (
+          <LtnAvailability stop={stop} avail={value} min={min} />
+        ))}
+      </table>
+    );
+  }
+}
+
+class LtnRequests extends Component<{ colon: string }> {
+  render(props: { colon: string }) {
+    const requests = stations()
+      .flatMap(([loc, stop]) => {
+        const wantedItems = colonMapCombinator(stop)[props.colon];
+        if (!(wantedItems < 0)) {
+          return [];
+        }
+        const computed = colonMapItems(stop)[props.colon];
+
+        const actualMinusWanted = computed ?? 0;
+        // want 100: wanted = -100
+        // actualMinusWanted -80 means there's 20 real items
+        // percentage satisfaction: 20/100 = 20%
+        return [
+          [[loc, stop], actualMinusWanted - wantedItems, -wantedItems] as const,
+        ];
+      })
+      .sort(
+        ([, avalue, awanted], [, bvalue, bwanted]) =>
+          avalue / awanted - bvalue / bwanted,
+      );
+    return (
+      <table className="ltn-avail">
+        {requests.map(([stop, value, wanted]) => (
+          <LtnAvailability
+            stop={stop}
+            avail={value}
+            min={wanted}
+            decimate={true}
+          />
+        ))}
+      </table>
+    );
+  }
+}
+
+class Storage extends Component<{ type: 'item' | 'fluid'; name: string }> {
+  render(props: { type: 'item' | 'fluid'; name: string }) {
+    return (
+      <ul>
+        {Object.entries(data.doc)
+          .map(
+            ([no, brick]) => [no, brick[`${props.type}s`][props.name]] as const,
+          )
+          .filter(([, count]) => count > 0)
+          .sort(([, a], [, b]) => b - a)
+          .map(([no, count]) => (
+            <li>
+              {humanise(count)} in <BlockLine block={no} />
+            </li>
+          ))}
+      </ul>
+    );
+  }
+}
+
+class RecipeUsage extends Component<{ type: string; name: string }> {
+  render(props: { type: string; name: string }) {
     const recipes = Object.entries(data.recipes).filter(
       ([, recipe]) =>
         undefined !==
@@ -59,145 +176,48 @@ export class IoFDetail extends Component<{
       );
     }
 
-    const colon = objToColon(props);
-    const providers = stations().flatMap(([loc, stop]) => {
-      const actualItemsAvailable = colonMapItems(stop)[colon];
-      if (!(actualItemsAvailable > 0)) {
-        return [];
-      }
+    return recipes
+      .sort(([a], [b]) => a.localeCompare(b))
+      .sort(([a], [b]) => totalAssemblersMaking(b) - totalAssemblersMaking(a))
+      .map(([name, recipe]) => {
+        const counts = dataByRecipe[name];
 
-      const settings = settingsMap(stop);
-      const min = ltnMinTransfer(colon, settings);
-      return [[[loc, stop], actualItemsAvailable, min] as const];
-    });
-    const requests = stations().flatMap(([loc, stop]) => {
-      const wantedItems = colonMapCombinator(stop)[colon];
-      if (!(wantedItems < 0)) {
-        return [];
-      }
-      const computed = colonMapItems(stop)[colon];
+        let inUse;
+        if (counts) {
+          inUse = (
+            <p>
+              Factory is using this recipe in:
+              <ul>
+                {Object.entries(counts.asms).map(([machine, count]) => (
+                  <li>
+                    {count} * <Item name={machine} />
+                  </li>
+                ))}
+              </ul>
+              In these blocks:
+              <ul>
+                {[...counts.blocks].map((block) => (
+                  <li>
+                    <BlockLine block={block} />
+                  </li>
+                ))}
+              </ul>
+            </p>
+          );
+        } else {
+          inUse = 'Factory does not use this recipe';
+        }
 
-      const actualMinusWanted = computed ?? 0;
-      // want 100: wanted = -100
-      // actualMinusWanted -80 means there's 20 real items
-      // percentage satisfaction: 20/100 = 20%
-      return [
-        [[loc, stop], actualMinusWanted - wantedItems, -wantedItems] as const,
-      ];
-    });
-    const storage = (
-      <p>
-        <h3>LTN availability:</h3>
-        <table className="ltn-avail">
-          {providers
-            .sort(
-              ([, avalue, amin], [, bvalue, bmin]) =>
-                bvalue / bmin - avalue / amin,
-            )
-            .map(([stop, value, min]) => (
-              <LtnAvailability stop={stop} avail={value} min={min} />
-            ))}
-        </table>
-        <h3>LTN requests:</h3>
-        <table className="ltn-avail">
-          {requests
-            .sort(
-              ([, avalue, awanted], [, bvalue, bwanted]) =>
-                avalue / awanted - bvalue / bwanted,
-            )
-            .map(([stop, value, wanted]) => (
-              <LtnAvailability
-                stop={stop}
-                avail={value}
-                min={wanted}
-                decimate={true}
-              />
-            ))}
-        </table>
-        <h3>Storage:</h3>
-        <ul>
-          {Object.entries(data.doc)
-            .map(
-              ([no, brick]) =>
-                [no, brick[`${props.type}s`][props.name]] as const,
-            )
-            .filter(([, count]) => count > 0)
-            .sort(([, a], [, b]) => b - a)
-            .map(([no, count]) => (
-              <li>
-                {humanise(count)} in <BlockLine block={no} />
-              </li>
-            ))}
-        </ul>
-      </p>
-    );
-
-    return (
-      <div class="container-fluid">
-        <h2>
-          <ItemIcon name={props.name} alt={props.name} /> {obj.localised_name}
-        </h2>
-        <p>
-          type: <span className="font-monospace">{props.type}</span>;
-          internal-name:<span class="font-monospace">{props.name}</span>; group:
-          <span class="font-monospace">{obj.group?.name}</span>; subgroup:
-          <span class="font-monospace">{obj.subgroup?.name}</span>.
-        </p>
-        <p>
-          <a href={`/ltn-tree/${props.type}/${props.name}`}>
-            Debug availability
-          </a>
-        </p>
-        {storage}
-        <h3>Ways to make:</h3>
-        <p>
-          {recipes
-            .sort(([a], [b]) => a.localeCompare(b))
-            .sort(
-              ([a], [b]) => totalAssemblersMaking(b) - totalAssemblersMaking(a),
-            )
-            .map(([name, recipe]) => {
-              const counts = dataByRecipe[name];
-
-              let inUse;
-              if (counts) {
-                inUse = (
-                  <p>
-                    Factory is using this recipe in:
-                    <ul>
-                      {Object.entries(counts.asms).map(([machine, count]) => (
-                        <li>
-                          {count} * <Item name={machine} />
-                        </li>
-                      ))}
-                    </ul>
-                    In these blocks:
-                    <ul>
-                      {[...counts.blocks].map((block) => (
-                        <li>
-                          <BlockLine block={block} />
-                        </li>
-                      ))}
-                    </ul>
-                  </p>
-                );
-              } else {
-                inUse = 'Factory does not use this recipe';
-              }
-
-              return (
-                <p>
-                  <h5>
-                    {recipe.localised_name} (
-                    <span class="font-monospace">{name}</span>)
-                  </h5>
-                  <p>{inUse}</p>
-                  <RecipeInOut name={name} />
-                </p>
-              );
-            })}
-        </p>
-      </div>
-    );
+        return (
+          <p>
+            <h5>
+              {recipe.localised_name} (
+              <span class="font-monospace">{name}</span>)
+            </h5>
+            <p>{inUse}</p>
+            <RecipeInOut name={name} />
+          </p>
+        );
+      });
   }
 }
