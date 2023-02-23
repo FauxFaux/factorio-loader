cc0c223251ec2892bf24f194ecfbf9a19bdbd69b
Chris West (Faux) <git@goeswhere.com>
2023-02-23 21:13:57 +0000

    chore: extract class

--- web/ltn-avail.tsx
+++ web/ltn-avail.tsx
@@ -1,7 +1,7 @@
 import { Component } from 'preact';
 
 import { StopLine } from './pages/station-status';
-import { Stat } from './muffler/stations';
+import { colonMapCombinator, colonMapItems, ltnMinTransfer, settingsMap, Stat, stations } from "./muffler/stations";
 import { Measurement } from './ltn-summary';
 import { humanise, humaniseNo } from './muffler/human';
 
@@ -66,3 +66,105 @@ export const LtnPercent = (props: Measurement & { decimate?: boolean }) => {
     </abbr>
   );
 };
+
+
+export type LtnFilter = { colon: string };
+
+export class LtnProvides extends Component<LtnFilter> {
+  render(props: LtnFilter) {
+    const providers = stations()
+      .flatMap(([loc, stop]) => {
+        const actualItemsAvailable = colonMapItems(stop)[props.colon];
+        if (!(actualItemsAvailable > 0)) {
+          return [];
+        }
+
+        const settings = settingsMap(stop);
+        const min = ltnMinTransfer(props.colon, settings);
+        const flow = stop.flowFrom[props.colon] ?? 0;
+        return [[[loc, stop], actualItemsAvailable, min, flow] as const];
+      })
+      .sort(
+        ([, avalue, amin], [, bvalue, bmin]) => bvalue / bmin - avalue / amin,
+      );
+
+    const totalFlow = providers.reduce((acc, [, , , flow]) => acc + flow, 0);
+
+    return (
+      <table className="ltn-avail">
+        <tr>
+          <th>
+            <abbr title="actual count available here right now, in the most recent snapshot">
+              act
+            </abbr>
+          </th>
+          <th>
+            <abbr title="what percentage of cargo was sourced/sunk from this stop, in the most recent *simulation*">
+              flow
+            </abbr>
+          </th>
+          <th>
+            <abbr title="how much of a train we can fill right now, according to the LTN settings and the latest snapshot">
+              imm
+            </abbr>
+          </th>
+          <th>stop / block</th>
+        </tr>
+        {providers.map(([stop, value, min, flow]) => (
+          <LtnAvailability
+            stop={stop}
+            avail={value}
+            min={min}
+            flows={{ flow, totalFlow }}
+          />
+        ))}
+      </table>
+    );
+  }
+}
+
+export class LtnRequests extends Component<LtnFilter> {
+  render(props: LtnFilter) {
+    const requests = stations()
+      .flatMap(([loc, stop]) => {
+        const wantedItems = colonMapCombinator(stop)[props.colon];
+        if (!(wantedItems < 0)) {
+          return [];
+        }
+        const computed = colonMapItems(stop)[props.colon];
+        const flow = stop.flowTo[props.colon] ?? 0;
+
+        const actualMinusWanted = computed ?? 0;
+        // want 100: wanted = -100
+        // actualMinusWanted -80 means there's 20 real items
+        // percentage satisfaction: 20/100 = 20%
+        return [
+          [
+            [loc, stop],
+            actualMinusWanted - wantedItems,
+            -wantedItems,
+            flow,
+          ] as const,
+        ];
+      })
+      .sort(
+        ([, avalue, awanted], [, bvalue, bwanted]) =>
+          avalue / awanted - bvalue / bwanted,
+      );
+    const totalFlow = requests.reduce((acc, [, , , flow]) => acc + flow, 0);
+
+    return (
+      <table className="ltn-avail">
+        {requests.map(([stop, value, wanted, flow]) => (
+          <LtnAvailability
+            stop={stop}
+            avail={value}
+            min={wanted}
+            decimate={true}
+            flows={{ flow, totalFlow }}
+          />
+        ))}
+      </table>
+    );
+  }
+}
--- web/pages/objects.tsx
+++ web/pages/objects.tsx
@@ -1,16 +1,9 @@
 import { Component } from 'preact';
 import { computed, data } from '../index';
-import { LtnAvailability } from '../ltn-avail';
+import { LtnProvides, LtnRequests } from "../ltn-avail";
 import { ItemIcon } from '../lists';
 import { BlockLine, Fluid, Item, ItemOrFluid } from '../objects';
 import { objToColon } from '../muffler/colon';
-import {
-  colonMapCombinator,
-  colonMapItems,
-  ltnMinTransfer,
-  settingsMap,
-  stations,
-} from '../muffler/stations';
 import { humanise } from '../muffler/human';
 
 export class IoFDetail extends Component<{
@@ -150,105 +143,6 @@ export class IoFDetail extends Component<{
           )}
         </div>
       </>
-    );
-  }
-}
-
-class LtnProvides extends Component<{ colon: string }> {
-  render(props: { colon: string }) {
-    const providers = stations()
-      .flatMap(([loc, stop]) => {
-        const actualItemsAvailable = colonMapItems(stop)[props.colon];
-        if (!(actualItemsAvailable > 0)) {
-          return [];
-        }
-
-        const settings = settingsMap(stop);
-        const min = ltnMinTransfer(props.colon, settings);
-        const flow = stop.flowFrom[props.colon] ?? 0;
-        return [[[loc, stop], actualItemsAvailable, min, flow] as const];
-      })
-      .sort(
-        ([, avalue, amin], [, bvalue, bmin]) => bvalue / bmin - avalue / amin,
-      );
-
-    const totalFlow = providers.reduce((acc, [, , , flow]) => acc + flow, 0);
-
-    return (
-      <table className="ltn-avail">
-        <tr>
-          <th>
-            <abbr title="actual count available here right now, in the most recent snapshot">
-              act
-            </abbr>
-          </th>
-          <th>
-            <abbr title="what percentage of cargo was sourced/sunk from this stop, in the most recent *simulation*">
-              flow
-            </abbr>
-          </th>
-          <th>
-            <abbr title="how much of a train we can fill right now, according to the LTN settings and the latest snapshot">
-              imm
-            </abbr>
-          </th>
-          <th>stop / block</th>
-        </tr>
-        {providers.map(([stop, value, min, flow]) => (
-          <LtnAvailability
-            stop={stop}
-            avail={value}
-            min={min}
-            flows={{ flow, totalFlow }}
-          />
-        ))}
-      </table>
-    );
-  }
-}
-
-class LtnRequests extends Component<{ colon: string }> {
-  render(props: { colon: string }) {
-    const requests = stations()
-      .flatMap(([loc, stop]) => {
-        const wantedItems = colonMapCombinator(stop)[props.colon];
-        if (!(wantedItems < 0)) {
-          return [];
-        }
-        const computed = colonMapItems(stop)[props.colon];
-        const flow = stop.flowTo[props.colon] ?? 0;
-
-        const actualMinusWanted = computed ?? 0;
-        // want 100: wanted = -100
-        // actualMinusWanted -80 means there's 20 real items
-        // percentage satisfaction: 20/100 = 20%
-        return [
-          [
-            [loc, stop],
-            actualMinusWanted - wantedItems,
-            -wantedItems,
-            flow,
-          ] as const,
-        ];
-      })
-      .sort(
-        ([, avalue, awanted], [, bvalue, bwanted]) =>
-          avalue / awanted - bvalue / bwanted,
-      );
-    const totalFlow = requests.reduce((acc, [, , , flow]) => acc + flow, 0);
-
-    return (
-      <table className="ltn-avail">
-        {requests.map(([stop, value, wanted, flow]) => (
-          <LtnAvailability
-            stop={stop}
-            avail={value}
-            min={wanted}
-            decimate={true}
-            flows={{ flow, totalFlow }}
-          />
-        ))}
-      </table>
     );
   }
 }
