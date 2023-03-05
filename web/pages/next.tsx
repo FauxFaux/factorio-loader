7aef06bc548c50e6e1d10fcbdbeec9fe61144f25
Chris West (Faux) <git@goeswhere.com>
2023-03-05 15:09:23 +0000

    feat: next knows 'new' items

--- web/pages/next.tsx
+++ web/pages/next.tsx
@@ -1,7 +1,8 @@
 import { Component } from 'preact';
 
 import { data } from '../datae';
-import { Recipe } from '../objects';
+import { ColonJoined, Recipe } from '../objects';
+import { objToColon } from '../muffler/colon';
 
 function stepsToUnlock(tech: string): number {
   const techData = data.technologies[tech];
@@ -16,25 +17,97 @@ export class Next extends Component<{}> {
     const techs = Object.entries(data.technologies)
       .filter(([name, tech]) => !tech.researched)
       .sort(([a], [b]) => stepsToUnlock(a) - stepsToUnlock(b));
+
+    const canMake = new Set(
+      Object.values(data.technologies)
+        .filter((tech) => tech.researched)
+        .flatMap((tech) =>
+          tech.unlocks.flatMap((recipe) =>
+            data.recipes[recipe].products.flatMap((p) => objToColon(p)),
+          ),
+        ),
+    );
+
+    // at 700 the browser/virtual dom crashes
+    const limit = 200;
     return (
       <div>
         <h1>Next</h1>
-        <ul>
-          {techs.map(([name, tech]) => (
-            <li>
-              {stepsToUnlock(name)}: {name} requires{' '}
-              {tech.requires
-                .filter((r) => !data.technologies[r].researched)
-                .join(', ')}
-              {tech.unlocks?.map((u) => {
-                const recipe = data.recipes[u];
-                recipe.ingredients;
-                return <Recipe name={u} />;
-              })}
-            </li>
-          ))}
-        </ul>
+        <table class={'table'}>
+          <thead>
+            <tr>
+              <th>nth</th>
+              <th>research name</th>
+              <th>new items</th>
+              <th>unlocked recipes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {techs.slice(0, limit).map(([name, tech]) => {
+              const products = [
+                ...new Set(
+                  tech.unlocks.flatMap((u) =>
+                    data.recipes[u].products.map((p) => objToColon(p)),
+                  ),
+                ),
+              ];
+              const newProducts = products.filter(
+                // (p) => (data.prodStats[p]?.output?.total ?? 0) === 0,
+                (p) => !canMake.has(p),
+              );
+              return renderTechRow(name, tech, newProducts);
+            })}
+            <tr>
+              <td colSpan={4}>...and {techs.length - limit} more</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
+}
+
+function renderTechRow(
+  name: string,
+  tech: { researched: boolean; requires: string[]; unlocks: string[] },
+  newProducts: string[],
+) {
+  return (
+    <tr>
+      <td>
+        {stepsToUnlock(name) === 1 ? (
+          '1'
+        ) : (
+          <abbr
+            title={tech.requires
+              .filter((r) => !data.technologies[r].researched)
+              .join(', ')}
+          >
+            {stepsToUnlock(name)}
+          </abbr>
+        )}
+      </td>
+      <td>{name}</td>
+      <td>
+        <ul>
+          {newProducts.map((p) => (
+            <li>
+              <ColonJoined label={p} />
+            </li>
+          ))}
+        </ul>
+      </td>
+      <td>
+        <ul>
+          {tech.unlocks?.map((u) => {
+            return (
+              <li>
+                <Recipe name={u} />
+              </li>
+            );
+          })}
+        </ul>
+      </td>
+    </tr>
+  );
 }
