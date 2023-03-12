5eb27cb8e12291a66e332c089b8acb49aff23f44
Chris West (Faux) <git@goeswhere.com>
2023-03-12 15:53:26 +0000

    feat: single level of pulses

--- web/pages/pulses.tsx
+++ web/pages/pulses.tsx
@@ -1,8 +1,9 @@
 import { Component } from 'preact';
 
-import * as ease from 'd3-ease';
+import { easeQuadInOut } from 'd3-ease';
 import { data, Loc, Pulse } from '../datae';
 import { useEffect } from 'preact/hooks';
+import { BlockLink } from './station-status';
 
 function topN<T>(n: number, entries: (readonly [T, number])[]) {
   entries.sort((a, b) => b[1] - a[1]);
@@ -18,38 +19,56 @@ const Line = (props: {
   total: number;
   w: number;
   y: number;
-  bricks: Record<Loc, number>;
+  liner: Liner[];
 }) => {
-  const top = topN(4, Object.entries(props.bricks));
-  const items = top.top
-    .map(([loc, amount]) => [amount, loc] as const)
-    .concat([[top.rest, 'rest']] as const);
-
-  return items.map(([ratio, loc], i) => {
-    const x =
-      (items
-        .slice(0, i)
-        .map(([r]) => r)
-        .reduce((a, b) => a + b, 0) *
-        props.w) /
-      props.total;
-    return (
-      <>
-        <rect
-          style={'opacity: 0.05'}
-          fill="red"
-          x={x}
-          width={(ratio * props.w) / props.total - 3}
-          y={props.y}
-          height="100"
-          rx="5"
-        />
-        <text x={x} y={props.y + 80} fill="black">
-          {loc}
-        </text>
-      </>
-    );
-  });
+  return (
+    <>
+      {props.liner.map((blob) => {
+        const ratio = blob.amount / props.total;
+        const x = (blob.off / props.total) * props.w;
+        console.log(blob, props);
+        const width = ratio * props.w - 3;
+        return (
+          <>
+            <rect
+              fill="red"
+              x={x}
+              width={width}
+              y={props.y}
+              height="100"
+              rx="5"
+            />
+            <text
+              x={x + width / 2}
+              y={props.y + 10}
+              text-anchor={'middle'}
+              fill="black"
+              font-size={'60%'}
+            >
+              {blob.names.slice(0, 4).map((loc, i) => (
+                <>
+                  <BlockLink loc={loc} />
+                  {i !== 3 ? '; ' : ''}
+                </>
+              ))}
+            </text>
+            <text
+              x={x + width / 2}
+              y={props.y + 20}
+              text-anchor={'middle'}
+              fill="black"
+              font-size={'60%'}
+            >
+              {blob.names
+                .map((loc) => data.doc[loc].tags.join('; '))
+                .join('; ')
+                .slice(0, 20)}
+            </text>
+          </>
+        );
+      })}
+    </>
+  );
 };
 
 export class Pulses extends Component<{ colon: string }> {
@@ -69,26 +88,68 @@ export class Pulses extends Component<{ colon: string }> {
 
     const w = 800;
 
+    const srcLiner = computeLine(sources);
+    const sinkLiner = computeLine(sinks);
+
+    function totalise(liners: Liner[]) {
+      return Object.fromEntries(
+        liners.flatMap(({ names, off, amount }) =>
+          names.map((name) => [name, [off / total, amount / total] as const]),
+        ),
+      );
+    }
+
     return (
       <div class={'row'}>
         <svg width={'100%'} viewBox={'0 0 800 1000'}>
-          <Line y={0} total={total} w={w} bricks={sources} />
-          <Line y={320} total={total} w={w} bricks={sinks} />
-          <Motion pulses={pulses} end={end} />
+          <Line y={0} total={total} w={w} liner={srcLiner} />
+          <Line y={320} total={total} w={w} liner={sinkLiner} />
+          <Motion
+            pulses={pulses}
+            end={end}
+            starts={totalise(srcLiner)}
+            ends={totalise(sinkLiner)}
+          />
         </svg>
       </div>
     );
   }
 }
 
-class Motion extends Component<
-  { pulses: Pulse[]; end: number },
-  { t: number }
-> {
-  render(props: { pulses: Pulse[]; end: number }, state: { t: number }) {
+interface Liner {
+  amount: number;
+  names: string[];
+  off: number;
+}
+
+function computeLine(bricks: Record<Loc, number>): Liner[] {
+  const top = topN(5, Object.entries(bricks));
+  const items = top.top
+    .map(([loc, amount]) => [amount, [loc] as string[]] as const)
+    .concat([[top.rest, top.names]] as const);
+
+  return items.map(([amount, names], i) => ({
+    amount,
+    names,
+    // sum of amounts up to this point (*not* divided by total)
+    off: items
+      .slice(0, i)
+      .map(([amount]) => amount)
+      .reduce((a, b) => a + b, 0),
+  }));
+}
+
+interface MotionProps {
+  pulses: Pulse[];
+  end: number;
+  starts: Record<Loc, readonly [number, number]>;
+  ends: Record<Loc, readonly [number, number]>;
+}
+class Motion extends Component<MotionProps, { t: number }> {
+  render(props: MotionProps, state: { t: number }) {
     useEffect(() => {
       const timer = setInterval(() => {
-        this.setState(({ t }) => ({ t: (t ?? 0) + 0.0001 }));
+        this.setState(({ t }) => ({ t: (t ?? 0.02) + 0.0001 }));
       }, 1000 / 60);
       return () => clearTimeout(timer);
     }, []);
@@ -106,23 +167,22 @@ class Motion extends Component<
         if (now < start || now > start + duration) {
           return [];
         }
-        const fromPair = from
-          .split(',')
-          .map((s) => 20 * (parseInt(s) + 10)) as [number, number];
-        const toPair = to.split(',').map((s) => 20 * (parseInt(s) + 10)) as [
-          number,
-          number,
-        ];
-        const x =
-          ease.easeQuadInOut((now - start) / duration) *
-            (toPair[0] - fromPair[0]) +
-          fromPair[0];
-        const y =
-          ease.easeQuadInOut((now - start) / duration) *
-            (toPair[1] - fromPair[1]) +
-          fromPair[1];
+        const w = 800;
+
+        const [sx, sw] = props.starts[from]!;
+        const [ex, ew] = props.ends[to]!;
+
+        const fx = w * (sx + sw / 2);
+        const fy = 95;
+
+        const tx = w * (ex + ew / 2);
+        const ty = 330;
+
+        const ease = easeQuadInOut((now - start) / duration);
+        const x = ease * (tx - fx) + fx;
+        const y = ease * (ty - fy) + fy;
         return (
-          <g>
+          <g key={`circ-${i}`}>
             <circle cx={x} cy={y} r={3 + Math.log(amount)} fill="red" />
           </g>
         );
