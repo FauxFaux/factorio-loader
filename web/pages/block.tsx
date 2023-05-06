b620f4621ed4c6db8afdafa969973bc6166b468d
Chris West (Faux) <git@goeswhere.com>
2023-05-06 17:33:45 +0100

    chore: move in/out calculation out of simulate

--- web/pages/block.tsx
+++ web/pages/block.tsx
@@ -12,10 +12,24 @@ import { Colon } from '../muffler/colon';
 import { cloneDeep } from 'lodash';
 import { stackSize } from './chestify';
 
-export function simulate(
-  asms: BlockContent['asms'],
-  include: (colon: Colon) => boolean = () => true,
-) {
+interface Modes {
+  // an unbounded amount of this is available from the rail network
+  inputs: Set<Colon>;
+
+  // an unbounded amount of this can be sunk by the rail network
+  outputs: Set<Colon>;
+
+  // this product may fill buffers, but, if buffers are full, the assemblers will jam
+  intermediates: Set<Colon>;
+
+  // an unbounded amount of this can be output by intermediate steps, but it is not an output
+  overflows: Set<Colon>;
+
+  // this ingredient is broken, do not consider it
+  ignore: Set<Colon>;
+}
+
+function toActions(asms: BlockContent['asms']): Record<string, number>[] {
   const actions: Record<Colon, number>[] = [];
   for (const [factory, recipeName, modules] of asms) {
     if (!recipeName) continue;
@@ -24,26 +38,16 @@ export function simulate(
     const scale = actualSpeed(factory, modules) / recp.time;
     actions.push(effectsOf(recp, scale));
   }
+  return actions;
+}
 
-  const outputs = new Set<Colon>();
-  const inputs = new Set<Colon>();
-  for (const action of actions) {
-    for (const [colon, count] of Object.entries(action)) {
-      if (count > 0) {
-        outputs.add(colon);
-      } else {
-        inputs.add(colon);
-      }
-    }
-  }
-
-  const wanted = [...inputs].filter((input) => !outputs.has(input));
+export function simulate(actions: Record<Colon, number>[], modes: Modes) {
   const START_VALUE = 1e9;
   const WARMUP = 5 * 60;
   const TICKS = 3600;
 
   const current: Record<Colon, number> = Object.fromEntries(
-    wanted.map((x) => [x, START_VALUE]),
+    [...modes.inputs].map((x) => [x, START_VALUE]),
   );
   const tick = () => {
     for (const action of actions) {
@@ -67,26 +71,9 @@ export function simulate(
     tick();
   }
 
-  return Object.entries(current)
-    .map(
-      ([colon, count]) =>
-        [colon, (count - initial[colon] ?? 0) / TICKS] as const,
-    )
-    .filter(([, count]) => Math.abs(count) > 0.1)
-    .sort(([, a], [, b]) => Math.abs(b) - Math.abs(a))
-    .filter(([colon]) => include(colon))
-    .map(([colon, count]) => {
-      const fullTrain = stackSize(colon) * 50;
-      return (
-        <li>
-          <span class={'amount'}>
-            {humanise((count * 60 * 60) / fullTrain)}tph
-          </span>
-          <span class={'amount'}>{humanise(count)}</span>/s{' '}
-          <ColonJoined colon={colon} />
-        </li>
-      );
-    });
+  return Object.entries(current).map(
+    ([colon, count]) => [colon, (count - initial[colon] ?? 0) / TICKS] as const,
+  );
 }
 
 export class BlockPage extends Component<{ loc: string }> {
@@ -131,6 +118,57 @@ export class BlockPage extends Component<{ loc: string }> {
 
     const { wanted, exports } = recipeDifference(obj);
 
+    let simulation;
+    {
+      const actions = toActions(obj.asms);
+
+      const outputs = new Set<Colon>();
+      const inputs = new Set<Colon>();
+      for (const action of actions) {
+        for (const [colon, count] of Object.entries(action)) {
+          if (count > 0) {
+            outputs.add(colon);
+          } else {
+            inputs.add(colon);
+          }
+        }
+      }
+
+      const wanted = [...inputs].filter((input) => !outputs.has(input));
+      const produced = [...outputs].filter((output) => !inputs.has(output));
+      const neither = [...outputs].filter(
+        (output) => !wanted.includes(output) && !produced.includes(output),
+      );
+      const modes: Modes = {
+        inputs: new Set(wanted),
+        outputs: new Set(produced),
+        intermediates: new Set(neither),
+        overflows: new Set(),
+        ignore: new Set(),
+      };
+
+      const result = simulate(actions, modes);
+
+      simulation = result
+        .filter(([, count]) => Math.abs(count) > 0.1)
+        .sort(([, a], [, b]) => Math.abs(b) - Math.abs(a))
+        .filter(
+          ([colon]) => modes.inputs.has(colon) || modes.outputs.has(colon),
+        )
+        .map(([colon, count]) => {
+          const fullTrain = stackSize(colon) * 50;
+          return (
+            <li>
+              <span class={'amount'}>
+                {humanise((count * 60 * 60) / fullTrain)}tph
+              </span>
+              <span class={'amount'}>{humanise(count)}</span>/s{' '}
+              <ColonJoined colon={colon} />
+            </li>
+          );
+        });
+    }
+
     return (
       <>
         <div class="row">
@@ -145,13 +183,8 @@ export class BlockPage extends Component<{ loc: string }> {
             <TrainStops stop={obj.stop} />
           </div>
           <div class="col">
-            <h3>Simulation (approved)</h3>
-            {simulate(
-              obj.asms,
-              (colon) => wanted.includes(colon) || exports.includes(colon),
-            )}
             <h3>Simulation</h3>
-            {simulate(obj.asms)}
+            {simulation}
 
             <h3 title="..but does not consume">Produces</h3>
             <ul>
