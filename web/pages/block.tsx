fcae39632077b6e8cc368d44cd6a2187688ebc4b
Chris West (Faux) <git@goeswhere.com>
2023-05-19 21:23:14 +0100

    feat: remove the old simulation

--- web/pages/block.tsx
+++ web/pages/block.tsx
@@ -9,8 +9,6 @@ import type { BlockContent } from '../../scripts/load-recs';
 import { makeUpRecipe } from '../muffler/walk-recipes';
 import { actualSpeed, effectsOf } from './plan';
 import { Colon, fromColon, splitColon } from '../muffler/colon';
-import { cloneDeep } from 'lodash';
-import { stackSize } from './chestify';
 import { ltnSummary } from '../ltn-summary';
 import { sortByKeys } from '../muffler/deter';
 import { ItemIcon } from '../lists';
@@ -115,7 +113,10 @@ function hillClimbMutates(
   return score;
 }
 
-export function findEfficiencies(realActions: Record<Colon, number>[], modes: Modes) {
+export function findEfficiencies(
+  realActions: Record<Colon, number>[],
+  modes: Modes,
+) {
   const actions = mergeDuplicateActions(realActions);
 
   const contributions = toContributions(actions);
@@ -161,140 +162,6 @@ export function findEfficiencies(realActions: Record<Colon, number>[], modes: Mo
   };
 }
 
-export function simulate(realActions: Record<Colon, number>[], modes: Modes) {
-  const WARMUP = 5 * 60;
-  const TICKS = 3600;
-  const OVERLOAD = 2;
-
-  // items involved in a cycle?
-  const required = new Set<Colon>([
-    'item:moss',
-    'item:biomass',
-    'fluid:carbon-dioxide',
-  ]);
-
-  const recipePriority = (action: Record<Colon, number>) => {
-    if (
-      Object.entries(action).some(
-        ([colon, count]) => count > 0 && required.has(colon),
-      )
-    ) {
-      return 2;
-    }
-
-    // this still deadlocks on auog butchery, __underflow hack below is closer but also deadlocks with the current stack sizes
-    const outputs = Object.entries(action)
-      .filter(([, count]) => count > 0)
-      .map(([colon]) => colon);
-    if (outputs.length === 1 && outputs[0] === 'item:cage') {
-      return -1;
-    }
-
-    return 0;
-  };
-
-  const actions = realActions
-    .map((action) => cloneDeep(action))
-    .sort((a, b) => recipePriority(b) - recipePriority(a));
-
-  for (const action of actions) {
-    const inputs = Object.entries(action)
-      .filter(([, count]) => count < 0)
-      .map(([colon]) => colon);
-
-    // sinkhole
-    if (inputs.length === 1 && inputs[0] === 'fluid:water') {
-      action['__overflow'] = 1;
-    }
-
-    const outputs = Object.entries(action)
-      .filter(([, count]) => count > 0)
-      .map(([colon]) => colon);
-    if (outputs.length === 1 && outputs[0] === 'item:cage') {
-      action['__underflow'] = 1;
-    }
-  }
-
-  const bufferSize: Record<Colon, number> = {};
-  for (const action of actions) {
-    for (const [colon, count] of Object.entries(action)) {
-      bufferSize[colon] = (bufferSize[colon] ?? 0) + OVERLOAD * Math.abs(count);
-    }
-  }
-
-  const current: Record<Colon, number> = Object.fromEntries(
-    Object.entries(bufferSize).map(([colon, value]) => [colon, value / 2]),
-  );
-  const overruns: Record<Colon, number> = {};
-  const underruns: Record<Colon, number> = {};
-
-  const tick = () => {
-    for (const action of actions) {
-      // if some input is missing, don't execute
-      const underran = Object.entries(action)
-        .filter(([colon]) => !colon.startsWith('_'))
-        .filter(([colon]) => !modes.inputs.has(colon))
-        .filter(
-          ([colon, count]) =>
-            count < (action['__overflow'] ? 0.5 : 0) * bufferSize[colon],
-        )
-        .find(([colon, count]) => current[colon] < -count);
-      if (underran) {
-        const [colon, count] = underran;
-        underruns[colon] = (underruns[colon] ?? 0) - count;
-        continue;
-      }
-
-      const overran = Object.entries(action)
-        .filter(([colon]) => !colon.startsWith('_'))
-        .filter(([colon]) => !modes.outputs.has(colon))
-        .filter(([, count]) => count > 0)
-        .find(
-          ([colon, count]) =>
-            current[colon] + count >
-            (action['__underflow'] ? 0.1 : 1) * bufferSize[colon],
-        );
-      if (overran) {
-        const [colon, count] = overran;
-        overruns[colon] = (overruns[colon] ?? 0) + count;
-        continue;
-      }
-      for (const [colon, count] of Object.entries(action).filter(
-        ([colon]) => !colon.startsWith('_'),
-      )) {
-        current[colon] = (current[colon] ?? 0) + count;
-      }
-    }
-  };
-  for (let i = 0; i < WARMUP; ++i) {
-    tick();
-  }
-  const initial = cloneDeep({
-    current,
-    overruns,
-    underruns,
-  });
-  for (let i = 0; i < TICKS; ++i) {
-    tick();
-  }
-
-  const scaleDown = (
-    obj: Record<Colon, number>,
-    initial: Record<Colon, number>,
-  ) =>
-    Object.entries(obj).map(
-      ([colon, count]) =>
-        [colon, (count - initial[colon] ?? 0) / TICKS] as const,
-    );
-
-  // you'd think TS could do this, wouldn't you, but no
-  return {
-    current: scaleDown(current, initial.current),
-    overruns: scaleDown(overruns, initial.overruns),
-    underruns: scaleDown(underruns, initial.underruns),
-  };
-}
-
 export class BlockPage extends Component<{ loc: string }> {
   render(props: { loc: string }) {
     const loc = props.loc;
@@ -337,7 +204,7 @@ export class BlockPage extends Component<{ loc: string }> {
 
     const { wanted, exports } = recipeDifference(obj);
 
-    let simulation, result, guessy, efficiencies;
+    let guessy, efficiencies: ReturnType<typeof findEfficiencies>;
     {
       const actions = toActions(obj.asms);
       for (let i = 0; i < obj.boilers; ++i) {
@@ -374,14 +241,15 @@ export class BlockPage extends Component<{ loc: string }> {
         modes.outputs.add(prov);
       }
 
-      result = simulate(actions, modes);
       efficiencies = findEfficiencies(actions, modes);
       guessy = (
         <>
           <ul>
             {efficiencies.actions.map((action, i) => (
               <li>
-                <span class={'amount'}>{Math.round(efficiencies.bestEfficiency[i] * 100)}%</span>{' '}
+                <span class={'amount'}>
+                  {Math.round(efficiencies.bestEfficiency[i] * 100)}%
+                </span>{' '}
                 {Object.entries(action)
                   .sort(([, a], [, b]) => a - b)
                   .map(([colon, count]) => {
@@ -399,24 +267,6 @@ export class BlockPage extends Component<{ loc: string }> {
           </ul>
         </>
       );
-
-      simulation = result.current
-        .sort(([, a], [, b]) => Math.abs(b) - Math.abs(a))
-        .filter(
-          ([colon]) => modes.inputs.has(colon) || modes.outputs.has(colon),
-        )
-        .map(([colon, count]) => {
-          const fullTrain = stackSize(colon) * 50;
-          return (
-            <li>
-              <span class={'amount'}>
-                {humanise((Math.abs(count) * 60 * 60) / fullTrain)}
-              </span>
-              <span class={'amount'}>{humanise(count)}</span>/s{' '}
-              <ColonJoined colon={colon} />
-            </li>
-          );
-        });
     }
 
     return (
@@ -433,39 +283,14 @@ export class BlockPage extends Component<{ loc: string }> {
             <TrainStops stop={obj.stop} />
           </div>
           <div class="col">
-            <h3>Guess</h3>
-            {guessy}
-            <h3>Simulation</h3>
-            {simulation}
-            <h3>Overruns</h3>
-            <ul>
-              {result.overruns
-                .sort(([, a], [, b]) => b - a)
-                .map(([colon, count]) => (
-                  <li>
-                    <span class={'amount'}>{humanise(count)}</span> &times;{' '}
-                    <ColonJoined colon={colon} />
-                  </li>
-                ))}
-            </ul>
-            <h3>Underruns</h3>
-            <ul>
-              {result.underruns
-                .sort(([, a], [, b]) => b - a)
-                .map(([colon, count]) => (
-                  <li>
-                    <span class={'amount'}>{humanise(count)}</span> &times;{' '}
-                    <ColonJoined colon={colon} />
-                  </li>
-                ))}
-            </ul>
-
             <h3 title="..but does not consume">Produces</h3>
             <ul>
               {exports.map((x) => (
                 <li>
-                  <span class={'amount'}>{humanise(efficiencies.result[x], { altSuffix: '/s'})}</span>
-                  <ColonJoined colon={x} />
+                  <span class={'amount'}>
+                    {humanise(efficiencies.result[x], { altSuffix: '/s' })}
+                  </span>
+                  /s <ColonJoined colon={x} />
                 </li>
               ))}
             </ul>
@@ -473,8 +298,10 @@ export class BlockPage extends Component<{ loc: string }> {
             <ul>
               {wanted.map((x) => (
                 <li>
-                  <span class={'amount'}>{humanise(efficiencies.result[x], { altSuffix: '/s'})}</span>
-                  <ColonJoined colon={x} />
+                  <span class={'amount'}>
+                    {humanise(efficiencies.result[x], { altSuffix: '/s' })}
+                  </span>
+                  /s <ColonJoined colon={x} />
                 </li>
               ))}
             </ul>
@@ -497,6 +324,12 @@ export class BlockPage extends Component<{ loc: string }> {
                 </li>
               ))}
             </ul>
+          </div>
+        </div>
+        <div class={'row'}>
+          <div class={'col'}>
+            <h3>Internal efficiencies</h3>
+            {guessy}
           </div>
         </div>
         <div class="row">
