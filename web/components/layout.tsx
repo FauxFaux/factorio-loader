5fc80a506a0adea91b729e6ef74b5e22b6784458
Chris West (Faux) <git@goeswhere.com>
2023-08-06 08:08:19 +0100

    wip: stations

--- web/components/layout.tsx
+++ web/components/layout.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'preact';
+import { Component, JSX } from 'preact';
 import { RecipeName } from '../muffler/walk-recipes';
 import { Colon } from '../muffler/colon';
 
@@ -47,10 +47,75 @@ interface RadarProps {
 class TileRadar extends Component<RadarProps> {
   render(props: RadarProps) {
     const { config } = props;
-    const { inStations, outStations, belts, assemblers } = config;
+    let { inStations, outStations, belts, assemblers } = config;
 
     const FLOOR = '#312a03';
     const RAIL = '#8c8e8b';
+
+    const c2 = ([svx, svy]: Pos, [evx, evy]: Pos, [ex, ey]: Pos) =>
+      `c ${svx} ${svy}, ${evx + ex} ${evy + ey}, ${ex} ${ey} `;
+
+    const rails = [
+      // inside corners
+      'M 4 13 a 8 8 0 0 1 8 -8',
+      'M 4 115 a 8 8 0 0 0 8 8',
+      'M 188 13 a 8 8 0 0 0 -8 -8',
+      'M 188 115 a 8 8 0 0 1 -8 8',
+      // outside curved track from the junctions
+      'M 4 4 a 8 8 0 0 0 -8 -8',
+      'M 188 4 a 8 8 0 0 1 8 -8',
+      'M 4 124 a 8 8 0 0 1 -8 8',
+      'M 188 124 a 8 8 0 0 0 8 8',
+    ];
+
+    inStations = 11;
+    outStations = 11;
+
+    const inRail = '4 13';
+    const stops: JSX.Element[] = [];
+
+    for (let i = 0; i < inStations; i++) {
+      const off = 8 * (i + 1);
+      const p = 8 + i;
+      rails.push(
+        `M ${inRail}`,
+        c2([0, p], [0, -p], [off, 20]),
+        'l 0 60',
+        c2([0, p], [0, -p], [-off, 20]),
+      );
+
+      stops.push(
+        <circle
+          cx={4 + off - 2}
+          cy={84}
+          r={1.8}
+          fill={'white'}
+          stroke={'black'}
+        />,
+      );
+    }
+
+    const outRail = '188 13';
+    for (let i = 0; i < outStations; i++) {
+      const off = -8 * (i + 1);
+      const p = 8 + i;
+      rails.push(
+        `M ${outRail}`,
+        c2([0, p], [0, -p], [off, 20]),
+        'l 0 60',
+        c2([0, p], [0, -p], [-off, 20]),
+      );
+
+      stops.push(
+        <circle
+          cx={188 + off + 2}
+          cy={38}
+          r={1.8}
+          fill={'white'}
+          stroke={'black'}
+        />,
+      );
+    }
 
     return (
       <svg viewBox="0 0 192 128" width={192 * 3} height={128 * 3}>
@@ -63,38 +128,12 @@ class TileRadar extends Component<RadarProps> {
           <rect x={0} y={128 - 4 - 2} width={192} height={2} fill={RAIL} />
           {/* inside curves, then outside curved track */}
           <path
-            d="
-          M 4 13 a 8 8 0 0 1 8 -8
-          M 4 115 a 8 8 0 0 0 8 8
-          M 188 13 a 8 8 0 0 0 -8 -8
-          M 188 115 a 8 8 0 0 1 -8 8
-          M 4 4 a 8 8 0 0 0 -8 -8
-          M 188 4 a 8 8 0 0 1 8 -8
-          M 4 124 a 8 8 0 0 1 -8 8
-          M 188 124 a 8 8 0 0 0 8 8
-          "
+            d={rails.join(' ')}
             stroke={RAIL}
             stroke-width={2}
             fill={'transparent'}
           />
-          {/* stations */}
-          {/*c 0 5, -5 -5, 8 13*/}
-          {/*c 5 5, 0 -5, 8 8*/}
-          <path
-            d="M 4 13
-          c 0 5, 5 5, 8 13
-          c 5 5, 0 5, 8 8
-          "
-            stroke={RAIL}
-            stroke-width={2}
-            fill={'transparent'}
-          />
-          <path
-            d="M 40 40 c 0 10, 30 0, 30 30 "
-            stroke="#8c8e8b"
-            stroke-width="2"
-            fill="transparent"
-          ></path>
+          {stops}
         </g>
       </svg>
     );
